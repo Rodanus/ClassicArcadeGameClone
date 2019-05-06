81285c28e5099c4ac3aefa252525ee6e5fd2e388
Roudan Haj Hussein <rodan.hajhussein31@gmail.com>
2019-05-06 20:44:44 +0300

    add needed properties to enemy class

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor() {
+    constructor(x, y, speed) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
-
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
