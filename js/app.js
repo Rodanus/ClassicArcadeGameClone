be293a8d2f87515e37b3fbf20e9e6dc533d72911
Roudan Haj Hussein <rodan.hajhussein31@gmail.com>
2019-05-06 20:59:26 +0300

    add player class with properties and empty methods

--- js/app.js
+++ js/app.js
@@ -30,7 +30,20 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player extends Enemy {
+    constructor(x, y) {
+        super(x, y);
+        this.sprite = 'images/char-boy.png';
+    }
 
+    update() {
+
+    }
+
+    handleInput(key) {
+
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
